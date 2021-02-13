1cd810effa638efc4c119fa7e2dbe1820f4e580d
Daniel Baynton <daniel.baynton@hotmail.com>
2021-02-13 19:35:26 +0000

    application learns to read json

--- src/page2.js
+++ src/page2.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Helmet } from 'react-helmet'
 import {Link } from "react-router-dom";
+import data from "./Configuration/List.json";
 
  function Page2() {
 	 return (
@@ -10,10 +11,29 @@ import {Link } from "react-router-dom";
 		</Helmet>
 		<h1>welcome to da house</h1>
 		<Link to="anything">
-			Make a 404
+			Make a 404<br></br>
 		</Link>
+		{data.list.map((item, i) =>{
+			return(
+				<>
+				{i} {item.name}: {item.description} <br></br>
+				</>
+			);
+		})}
+		{renderOffer()}
 	 </>
 	 );
  }
+
+ function renderOffer(){
+	if(data.offer){
+		return(
+		   <h1>Offer on!!</h1>
+		);
+	}
+	else{
+		return;
+	}
+}
 
  export default Page2;
